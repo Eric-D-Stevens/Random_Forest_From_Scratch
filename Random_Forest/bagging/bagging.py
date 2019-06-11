ea698188f848040d868aca61408061ee5b8c1954
Eric Stevens <ericstevens26101@gmail.com>
2019-06-10 22:22:24 -0700

    start bagging

---
+++ Random_Forest/bagging/bagging.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class bagging:
+
+    def __init__(self,
+                 data: np.ndarray,
+                 bags: int,
+                 bag_size: int,
+                 num_features: int,
+                 bootstrap_features: bool = False):
+
+       pass
+
+
+
+
+class bag:
+
+    def __init__(self,):
+
